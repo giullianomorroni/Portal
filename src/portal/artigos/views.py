8cfce5683a35e8f18185dbf7e3fdb81a8c8e1fa4
giullianomorroni <giullianomorroi@gmail.com>
2012-06-26 23:37:30 -0300

    Atualizacoes

--- src/portal/artigos/views.py
+++ src/portal/artigos/views.py
@@ -2,7 +2,7 @@
 #from django.contrib.auth import authenticate,  login,  logout
 #from django.contrib.auth.models import User,  UserManager
 from django.shortcuts import render_to_response
-from models import Topico, Artigo, Noticia;
+from models import Topico, Artigo, Noticia, Comentario;
 from posicao.gerador import gerador;
 #from django.http import HttpResponse
 #from django.http import HttpResponseRedirect
@@ -26,7 +26,23 @@ def topico(request, url):
 def artigo(request, url):
     print url;
     artigo = Artigo.objects.get(url=url)
-    return render_to_response('artigo.html',  {'artigo' : artigo},  context_instance=RequestContext(request));
+    comentarios = ['Nenhum comentario'];
+    try:
+        comentarios = Comentario.objects.filter(artigo = artigo.id)
+    except Comentario.DoesNotExist:
+        print 'nenhum comentario';
+    return render_to_response('artigo.html',  {'artigo' : artigo, 'comentarios':comentarios},  context_instance=RequestContext(request));
 
 def sobre(request):
     return render_to_response('sobre.html');
+
+def novo_comentario(request):
+    c = Comentario();
+    c.autor = request.POST['autor'];
+    c.comentario = request.POST['comentario'];
+    artigo = Artigo.objects.get(id = request.POST['id_artigo']);
+    print artigo.id; 
+    c.artigo = artigo;
+    c.save();
+    comentarios = Comentario.objects.all(artigo = artigo.id)
+    return render_to_response('artigo.html',  {'artigo' : artigo, 'comentarios':comentarios},  context_instance=RequestContext(request));
